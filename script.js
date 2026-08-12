be9c7dacc405ecdb5b094a0c27bd976ee50c24a0
TalRash2b <dmitry.bobarikov@gmail.com>
2026-08-13 00:50:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -282,6 +282,7 @@ function showInfo(aptNum, floor, residents) {
 
 // 8. Закрытие панели
 function closeInfo() {
+    alert('Крестик сработал!');
     console.log('❌ closeInfo вызвана!');
     const panel = document.getElementById('infoPanel');
     const overlay = document.getElementById('overlay');
